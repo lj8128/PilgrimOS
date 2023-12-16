3429a9d6b46fe12df929df3a7a6b774399aad507
Jongsuh Lee (James) <leejs8128@gmail.com>
2023-12-16 16:25:19 -0500

    Provision memory for tertiary bootloader, which will be used to transition to protected mode.

--- src/bootloader/bl_tertiary.asm
+++ src/bootloader/bl_tertiary.asm
@@ -17,7 +17,30 @@ main:
     mov ah, 0xE
     mov al, 'Y'
     mov bh, 0
-    int 0x10 
+    int 0x10
     jmp $
+
+gdt_null_segment_descriptor:
+    dq 0x0
+
+gdt_code_segment_descriptor:
+    dw 0xFFFF                ; Limit, first part (lower 16 bits)
+    dw 0x0                   ; Base, first part (lower 16 bits)
+    db 0x0                   ; Base, second part (middle 8 bits)
+    db 1001_1011b            ; Access Byte
+    db 1100_1111b            ; Flags and Limit, second part (upper 4 bits)
+    db 0x0                   ; Base, third part (upper 8 bits)
+
+gdt_data_segment_descriptor:
+    dw 0xFFFF                ; Limit, first part (lower 16 bits)
+    dw 0x0                   ; Base, first part (lower 16 bits)
+    db 0x0                   ; Base, second part (middle 8 bits)
+    db 1000_0011b            ; Access Byte
+    db 1100_1111b            ; Flags and Limit, second part (upper 4 bits)
+    db 0x0                   ; Base, third part (upper 8 bits)
+
+gdt_descriptor:
+    dw gdt_null_segment_descriptor - gdt_descriptor - 1;
+    dd gdt_null_segment_descriptor
 
 zero_fill       times 512-($-$$) db 0
